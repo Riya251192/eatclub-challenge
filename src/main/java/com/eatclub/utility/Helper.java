abae12ed0c41ecce71f5aa68b695b95dc4656616
riya <sinha.riya2511@gmail.com>
2025-06-04 21:03:36 +1000

    using Objects.nonNull

--- src/main/java/com/eatclub/utility/Helper.java
+++ src/main/java/com/eatclub/utility/Helper.java
@@ -6,18 +6,19 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatterBuilder;
 import java.util.Locale;
+import java.util.Objects;
 
 @Component
 public class Helper {
 
     public LocalTime retrieveLocalTime(String timeOfDay) {
-        if (timeOfDay != null && timeOfDay.length() < 7) timeOfDay = '0' + timeOfDay;
+        if (Objects.nonNull(timeOfDay) && timeOfDay.length() < 7) timeOfDay = '0' + timeOfDay;
         LocalTime queryTime = LocalTime.parse(timeOfDay, new DateTimeFormatterBuilder().parseCaseInsensitive().appendPattern("hh:mma").toFormatter(Locale.US));
         return queryTime;
     }
 
     public LocalDateTime retrieveLocalDateTime(String timeOfDay) {
-        if (timeOfDay != null && timeOfDay.length() < 7) timeOfDay = '0' + timeOfDay;
+        if (Objects.nonNull(timeOfDay) && timeOfDay.length() < 7) timeOfDay = '0' + timeOfDay;
         LocalDate date = LocalDate.now();
 
         LocalTime queryTime = LocalTime.parse(timeOfDay, new DateTimeFormatterBuilder().parseCaseInsensitive().appendPattern("hh:mma").toFormatter(Locale.US));
