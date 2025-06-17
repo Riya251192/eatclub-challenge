74a7ade584a6eb58be612ebdd08e61acdfb73387
riya <sinha.riya2511@gmail.com>
2025-06-17 18:58:52 +1000

    Exception for time

--- src/main/java/com/eatclub/utility/Helper.java
+++ src/main/java/com/eatclub/utility/Helper.java
@@ -1,5 +1,6 @@
 package com.eatclub.utility;
 
+import com.eatclub.exception.BadRequestException;
 import org.springframework.stereotype.Component;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -12,17 +13,25 @@ import java.util.Objects;
 public class Helper {
 
     public LocalTime retrieveLocalTime(String timeOfDay) {
-        if (Objects.nonNull(timeOfDay) && timeOfDay.length() < 7) timeOfDay = '0' + timeOfDay;
-        LocalTime queryTime = LocalTime.parse(timeOfDay, new DateTimeFormatterBuilder().parseCaseInsensitive().appendPattern("hh:mma").toFormatter(Locale.US));
-        return queryTime;
+        try {
+            if (Objects.nonNull(timeOfDay) && timeOfDay.length() < 7) timeOfDay = '0' + timeOfDay;
+            LocalTime queryTime = LocalTime.parse(timeOfDay, new DateTimeFormatterBuilder().parseCaseInsensitive().appendPattern("hh:mma").toFormatter(Locale.US));
+            return queryTime;
+        }catch (Exception ex){
+            throw new BadRequestException("Format incorrect for time :"+timeOfDay);
+        }
     }
 
     public LocalDateTime retrieveLocalDateTime(String timeOfDay) {
+        try {
         if (Objects.nonNull(timeOfDay) && timeOfDay.length() < 7) timeOfDay = '0' + timeOfDay;
         LocalDate date = LocalDate.now();
 
-        LocalTime queryTime = LocalTime.parse(timeOfDay, new DateTimeFormatterBuilder().parseCaseInsensitive().appendPattern("hh:mma").toFormatter(Locale.US));
+        LocalTime queryTime = LocalTime.parse(timeOfDay, new DateTimeFormatterBuilder().parseCaseInsensitive().appendPattern("hh:mma").toFormatter());
         LocalDateTime localDateTime = LocalDateTime.of(date, queryTime);
         return localDateTime;
+        }catch (Exception ex){
+            throw new BadRequestException("Format incorrect for time :"+timeOfDay);
+        }
     }
 }
